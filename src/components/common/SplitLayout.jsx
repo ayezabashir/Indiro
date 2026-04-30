ced78561a50678de548c3ccc6187b60520e285a4
Ayeza Bashir <ayezabashir46@gmail.com>
2026-04-30 10:58:39 +0500

    split layout

--- src/components/common/SplitLayout.jsx
+++ src/components/common/SplitLayout.jsx
@@ -9,18 +9,30 @@ const SplitLayout = ({
 }) => {
   return (
     <section
-      className={`relative min-h-screen grid grid-cols-1 bg-brand-navy overflow-hidden ${containerClassName}`}
+      className={`relative min-h-screen grid grid-cols-1 lg:grid-cols-2 bg-brand-navy overflow-hidden ${containerClassName}`}
     >
+      <div className="absolute inset-0 z-0 pointer-events-none transition-opacity duration-1000">
+        <div
+          className="absolute inset-0 block lg:hidden"
+          style={{
+            backgroundImage: `linear-gradient(to right, rgba(2, 6, 23, 0.7) 0%, var(--color-brand-navy) 100%), url('${bgImage}')`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+
+        <div
+          className="absolute inset-0 hidden lg:block"
+          style={{
+            backgroundImage: `linear-gradient(to right, transparent 0%, var(--color-brand-navy) 20%), url('${bgImage}')`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+      </div>
+
       <div
-        className="absolute inset-0 z-0 pointer-events-none transition-opacity duration-1000"
-        style={{
-          backgroundImage: `linear-gradient(to right, transparent 0%, var(--color-brand-navy) 20%), url('${bgImage}')`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      ></div>
-      <div
-        className={`flex flex-col justify-center px-8 lg:px-16 py-20 relative z-10 ${leftClassName}`}
+        className={`relative lg:static flex flex-col justify-center px-8 lg:px-16 py-20 z-10 ${leftClassName}`}
       >
         <div
           className={`transition-all duration-500 ease-out ${
@@ -33,7 +45,7 @@ const SplitLayout = ({
         </div>
       </div>
       <div
-        className={`relative h-150 lg:h-auto ${rightClassName}`}
+        className={`relative lg:static h-auto flex flex-col justify-center z-10 ${rightClassName}`}
       >
         <div
           className={`h-full w-full transition-all duration-700 ease-in-out ${
